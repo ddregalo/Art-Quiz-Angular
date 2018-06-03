88c1225cc5795309dbfbfdd01cdd5856937b3e41
danielcampos <ddregalo@gmail.com>
2018-06-03 22:50:45 +0100

    Implement answered question and active question +  continue button

--- javascripts/controllers/quiz.js
+++ javascripts/controllers/quiz.js
@@ -10,10 +10,31 @@
       self.quizMetrics = quizMetrics;
       self.dataService = DataService;
       self.activeQuestion = 0;
+      self.setActiveQuestion = setActiveQuestion;
       self.questionAnswered = questionAnswered;
 
+      var numQuestionsAnswered = 0;
+
+      function setActiveQuestion () {
+        var breakOut = false;
+        var quizLength = DataService.quizQuestions.length - 1;
+        while (!breakOut) {
+          self.activeQuestion = self.activeQuestion < quizLength ? ++self.activeQuestion : 0;
+          if (DataService.quizQuestions[self.activeQuestion].selected === null) {
+            breakOut = true;
+          }
+        }
+      }
+
       function questionAnswered() {
-        
+        var quizLength = DataService.quizQuestions.length;
+        if(DataService.quizQuestions[self.activeQuestion.selected !== null]) {
+          numQuestionsAnswered ++;
+          if(numQuestionsAnswered >= quizLength) {
+            // finalize quiz
+          }
+        }
+        self.setActiveQuestion();
       }
     }
 
