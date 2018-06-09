72d54c8bba7e4c345119199afd5596c2f05be371
danielcampos <ddregalo@gmail.com>
2018-06-09 11:14:54 +0100

    Display quiz results by question with correct answers

--- javascripts/controllers/results.js
+++ javascripts/controllers/results.js
@@ -10,5 +10,15 @@
 
       qz.quizMetrics = quizMetrics;
       qz.dataService = DataService;
+      qz.activeQuestion = 0;
+      qz.getAnswerClass = getAnswerClass;
+
+      function getAnswerClass(answerIndex) {
+          if(answerIndex === quizMetrics.correctAnswers[qz.activeQuestion]) {
+          return "bg-success";
+        } else if(answerIndex === DataService.quizQuestions[qz.activeQuestion].selected) {
+          return "bg-danger";
+        }
+      }
     }
 })();
